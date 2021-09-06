313cc4be8f95b6ac4101ea68ae3d6c6d39d06f90
lferrara <leo.ferrara@davikingcode.com>
2021-09-06 16:34:06 +0200

    fix: uppercase issue for the autocomplete css

--- src/stories/AutoComplete.stories.tsx
+++ src/stories/AutoComplete.stories.tsx
@@ -1,106 +1,113 @@
-import React, { FC } from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React, { FC } from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import Autocomplete, { AutocompleteProps } from '../components/Autocomplete';
-import './Autocomplete.css';
+import Autocomplete, { AutocompleteProps } from "../components/Autocomplete";
+import "./AutoComplete.css";
 
 export default {
-    title: 'Components/AutoComplete',
-    component: Autocomplete,
+  title: "Components/AutoComplete",
+  component: Autocomplete,
 } as ComponentMeta<typeof Autocomplete>;
 
-
 async function sleep(time: number) {
-    return new Promise(resolve => setTimeout(resolve, time));
+  return new Promise((resolve) => setTimeout(resolve, time));
 }
 
 async function fakeApiLoad<T>(data: T, delayMs: number = 2000) {
-    await sleep(delayMs);
-    return data;
+  await sleep(delayMs);
+  return data;
 }
 
-const Template: ComponentStory<typeof Autocomplete> = (args: AutocompleteProps<any, any, any, any>) => {
-    return <form style={{ maxWidth: '600px' }}>
-        <label>{args.label}</label>
-        <Autocomplete {...args} />
-    </form>;
-}
+const Template: ComponentStory<typeof Autocomplete> = (
+  args: AutocompleteProps<any, any, any, any>
+) => {
+  return (
+    <form style={{ maxWidth: "600px" }}>
+      <label>{args.label}</label>
+      <Autocomplete {...args} />
+    </form>
+  );
+};
 
 const entreprises = [
-    { nom: 'Da Viking Code', id: '1' },
-    { nom: 'Enedis', id: '2' },
-    { nom: 'Etalab', id: '3' },
-    { nom: 'Sibiluk', id: '4' },
+  { nom: "Da Viking Code", id: "1" },
+  { nom: "Enedis", id: "2" },
+  { nom: "Etalab", id: "3" },
+  { nom: "Sibiluk", id: "4" },
 ];
 
 export const Default = Template.bind({});
 Default.args = {
-    label: "Entreprise",
-    asyncSearchFn: () => fakeApiLoad(entreprises),
-    renderOption: (option: any) => <> {option.nom} </>,
-    getOptionLabel: (option: any) => option.nom,
-    inputProps: {
-        name: 'entreprise',
-    }
-}
+  label: "Entreprise",
+  asyncSearchFn: () => fakeApiLoad(entreprises),
+  renderOption: (option: any) => <> {option.nom} </>,
+  getOptionLabel: (option: any) => option.nom,
+  inputProps: {
+    name: "entreprise",
+  },
+};
 
 export const Multiple = Template.bind({});
 Multiple.args = {
-    label: "Entreprises",
-    multiple: true,
-    asyncSearchFn: () => fakeApiLoad(entreprises),
-    renderOption: (option: any) => <> {option.nom} </>,
-    getOptionLabel: (option: any) => option.nom
-}
+  label: "Entreprises",
+  multiple: true,
+  asyncSearchFn: () => fakeApiLoad(entreprises),
+  renderOption: (option: any) => <> {option.nom} </>,
+  getOptionLabel: (option: any) => option.nom,
+};
 
 const users = [
-    {
-        identity: {
-            name: 'Dupont',
-            first_name: 'Jean',
-        },
-        contact_point: {
-            email: 'jean.dupont@test.fr',
-        },
-        organization: {
-            employer: 'Bouygues Télécom',
-        },
+  {
+    identity: {
+      name: "Dupont",
+      first_name: "Jean",
     },
-    {
-        identity: {
-            name: 'Dupont',
-            first_name: 'Bertrand',
-        },
-        contact_point: {
-            email: 'bertrand.dupont@test.fr',
-        },
-        organization: {
-            employer: 'Bouygues Télécom',
-        },
-    }
+    contact_point: {
+      email: "jean.dupont@test.fr",
+    },
+    organization: {
+      employer: "Bouygues Télécom",
+    },
+  },
+  {
+    identity: {
+      name: "Dupont",
+      first_name: "Bertrand",
+    },
+    contact_point: {
+      email: "bertrand.dupont@test.fr",
+    },
+    organization: {
+      employer: "Bouygues Télécom",
+    },
+  },
 ];
 
-const UserOption: FC<{ user: any }> = ({ user }) =>
+const UserOption: FC<{ user: any }> = ({ user }) => (
+  <div>
     <div>
-        <div>{user.identity.name} {user.identity.first_name}</div>
-        <div>{user.contact_point.email}</div>
-        <div>{user.organization.employer}</div>
+      {user.identity.name} {user.identity.first_name}
     </div>
+    <div>{user.contact_point.email}</div>
+    <div>{user.organization.employer}</div>
+  </div>
+);
 
-const UserOptionLabel = ({ user }: { user: any }) => user.identity.name + ' ' + user.identity.first_name
+const UserOptionLabel = ({ user }: { user: any }) =>
+  user.identity.name + " " + user.identity.first_name;
 
 export const ComplexOption = Template.bind({});
 ComplexOption.args = {
-    label: "User",
-    asyncSearchFn: () => fakeApiLoad(users),
-    renderOption: (option: any) => <UserOption user={option} />,
-    getOptionLabel: (option: any) => UserOptionLabel({ user: option })
-}
+  label: "User",
+  asyncSearchFn: () => fakeApiLoad(users),
+  renderOption: (option: any) => <UserOption user={option} />,
+  getOptionLabel: (option: any) => UserOptionLabel({ user: option }),
+};
 
 export const Synchronous = Template.bind({});
 Synchronous.args = {
-    label: "Synchronous",
-    asyncSearchFn: () => fakeApiLoad(users, 0),
-    renderOption: (option: any) => <UserOption user={option} />,
-    getOptionLabel: (option: any) => UserOptionLabel({ user: option })
-}
+  label: "Synchronous",
+  asyncSearchFn: () => fakeApiLoad(users, 0),
+  renderOption: (option: any) => <UserOption user={option} />,
+  getOptionLabel: (option: any) => UserOptionLabel({ user: option }),
+};
