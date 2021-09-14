9d70b8559e0db2bf048ab2487a954dc523697fbd
lferrara <leo.ferrara@davikingcode.com>
2021-09-14 12:07:39 +0200

    Revert "chore(release): 1.6.1 [skip ci]"
    This reverts commit ca0efb8ccaa8bf66a3394308d3fab206364ab868.

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,9 @@
 import resolve from "@rollup/plugin-node-resolve";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import typescript from "@rollup/plugin-typescript";
 import del from 'rollup-plugin-delete';
 import svgr from '@svgr/rollup';
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export default [
     // CommonJS
@@ -17,6 +19,13 @@ export default [
         },
         plugins: [
             del({ targets: 'lib/*' }),
+            typescript({
+                tsconfig: './tsconfig.lib.json',
+                declaration: true,
+                declarationDir: 'types/',
+                rootDir: 'src/',
+                noEmit: true
+            }),
             peerDepsExternal(),
             resolve(),
             svgr()
