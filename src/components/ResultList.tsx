a070df3d5e93328e40e74cba2359d6dcecdb2206
lferrara <leo.ferrara@davikingcode.com>
2021-09-14 12:10:12 +0200

    fix: revert

--- src/components/ResultList.tsx
+++ src/components/ResultList.tsx
@@ -3,39 +3,50 @@ import React, { FC } from "react";
 import styled from "styled-components";
 
 const ActualResultListWrapper = styled.ul`
-    list-style: none;
-    padding: 0;
-    margin: 0;
+  list-style: none;
+  padding: 0;
+  margin: 0;
 
-    & > li {
-        margin: 0;
-    }
+  & > li {
+    margin: 0;
+  }
 `;
 
-const ActualResultList: FC<{ Results: React.ReactNodeArray }> = ({ Results }) => (
-    <ActualResultListWrapper>{
-        Results.map((Result, i) => <li key={i}>
-            {Result}
-            <Divider />
-        </li>)
-    }</ActualResultListWrapper>
+const ActualResultList: FC<{ Results: React.ReactNodeArray }> = ({
+  Results,
+}) => (
+  <ActualResultListWrapper>
+    {Results.map((Result, i) => (
+      <li key={i}>
+        {Result}
+        <Divider />
+      </li>
+    ))}
+  </ActualResultListWrapper>
 );
 
 export interface ResultListProps {
-    children?: undefined;
-    loading: boolean;
-    Results: React.ReactNodeArray;
-    LoadingSplash: React.ReactNode;
-    NoResultSplash: React.ReactNode;
+  children?: undefined;
+  loading: boolean;
+  Results: React.ReactNodeArray;
+  LoadingSplash: React.ReactNode;
+  NoResultSplash: React.ReactNode;
 }
 
-export const ResultList: FC<ResultListProps> = ({ loading, LoadingSplash, NoResultSplash, Results }) => {
-    if (loading) {
-        return <>{LoadingSplash}</>;
-    }
-    return (Results && Results.length > 0)
-        ? <ActualResultList Results={Results} />
-        : <>{NoResultSplash}</>;
+export const ResultList: FC<ResultListProps> = ({
+  loading,
+  LoadingSplash,
+  NoResultSplash,
+  Results,
+}) => {
+  if (loading) {
+    return <>{LoadingSplash}</>;
+  }
+  return Results && Results.length > 0 ? (
+    <ActualResultList Results={Results} />
+  ) : (
+    <>{NoResultSplash}</>
+  );
 };
 
 export default ResultList;
