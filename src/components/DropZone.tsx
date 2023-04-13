26a51918482ec9d311cd2be52671ece561871d7f
Camille Meunier <camille.meunier@davikingcode.com>
2023-04-13 10:23:41 +0200

    fix: Dropzone add callback method

--- src/components/DropZone.tsx
+++ src/components/DropZone.tsx
@@ -36,6 +36,7 @@ export interface DropZoneProps {
   // callback api 
   uploadSuccessCallback?: (files: FileList) => boolean,
   uploadErrorCallback?: (files: FileList, error: string) => boolean,
+  addCallBack? : () => void,
   dropzoneParams: any
 }
 
@@ -61,6 +62,7 @@ export const DropZone = React.forwardRef<HTMLFormElement, DropZoneProps>(
       token,
       url,
       redirect_url,
+      addCallBack = () => {},
       dropzone_index,
       accepted_files,
       accepted_extensions,
@@ -115,7 +117,7 @@ export const DropZone = React.forwardRef<HTMLFormElement, DropZoneProps>(
     };
 
     // State of dropzone object
-    const [dropZone, setDropZone] = useState<Dropzone | null>(null);
+    const [dropZone, setDropZone] = useState<Dropzone | undefined>(undefined);
     const [isHidden, setIsHidden] = useState(true);
     const [isInfoHidden, setIsInfoHidden] = useState(true);
 
@@ -124,76 +126,6 @@ export const DropZone = React.forwardRef<HTMLFormElement, DropZoneProps>(
 
     // FormRef
     const formRef = useRef<HTMLFormElement>(null);
-
-    // Listeners
-    dropZone &&
-      (dropZone as Dropzone).on("addedfiles", () => {
-        if (!autoProcessQueue) {
-          // Hide custom progress bar when add files
-          $("#upload_form-" + dropzoneIndex + " .custom-dz-upload").hide();
-          setIsHidden(false);
-          setIsInfoHidden(true);
-        }
-
-      }) &&
-      (dropZone as Dropzone).on("processing", (file) => {
-        setIsHidden(true);
-        setIsInfoHidden(true);
-        // Hide default progress bar when processing
-        $(file.previewElement).find(".dz-upload").hide();
-        // Show custom progress bar when processing
-        $(file.previewElement).find(".custom-dz-upload").show();
-        $(file.previewElement).find(".dz-progress").css("height", "inherit");
-
-      }) &&
-      (dropZone as Dropzone).on("success", (file) => {
-        // Hide custom progress bar when processing
-        $(file.previewElement).find(".custom-dz-upload").hide();
-        // Override css success mark
-        $(file.previewElement).find(".dz-success-mark").css("animation", "slide-in 3s cubic-bezier(0.77, 0, 0.175, 1)");
-        $(file.previewElement).find(".dz-success-mark").css("opacity", "1");
-
-        if (!autoProcessQueue) {
-          // ManualProcess context : Is there file left to process ?
-          if (dropZone.getQueuedFiles().length > 0) {
-            dropZone.processQueue();
-          } else {
-            setIsInfoHidden(false);
-          }
-        } else {
-          // AutoProcess context : Refresh button available
-          setIsHidden(false);
-          setIsInfoHidden(true);
-        }
-      }) && 
-      (dropZone as Dropzone).on("error", () => {
-        setIsInfoHidden(true);
-        setIsHidden(true);
-      }) && 
-	  (dropZone as Dropzone).on("sending", (file: any, xhr, formData) => {
-  
-		console.log(file, xhr, formData);
-
-		if (dropZoneParameter.chunking) {
-			formData.append('dzuuid', file.upload?.uuid);
-			formData.append('dztotalchunkcount', file.upload?.totalChunkCount);
-
-			let index = 1;
-			// @ts-nocheck
-			if(file.upload?.chunks)
-			{
-			  index = file.upload.chunks[file.upload.chunks.length - 1].index
-			}
-			
-			formData.append('dzchunkindex', index.toString());
-			formData.append('dztotalfilesize', file.size );
-			formData.append('dzchunksize', dropZoneParameter.chunkSize.toString());
-		}
-	  }) && 
-	  (dropZone as Dropzone).on("chunksUploaded", (file, done) => {
-		console.log(file);
-		done();
-	  });
 
     // Initialization of DropZone
     useEffect(() => {
@@ -205,40 +137,100 @@ export const DropZone = React.forwardRef<HTMLFormElement, DropZoneProps>(
             ...dropZoneParameter,
             previewTemplate: ReactDOMServer.renderToString(DropZonePreviewTemplate),
             error: (file: any, message: any) => {
-              // Code from github "npm dropzone"
-              if (file.upload && file.upload.uuid && file.previewElement) {
-                file.previewElement.classList.add("dz-error");
-                if (typeof message !== "string" && (message.error || message.file)) {
-                  message = message.error || message.file;
-                } else if (typeof message !== "string") {
-                  // Custom error sent by api
-                  var finalMessage: string[] = [];
+				// Code from github "npm dropzone"
+				if (file.upload && file.upload.uuid && file.previewElement) {
+					file.previewElement.classList.add("dz-error");
+					if (typeof message !== "string" && (message.error || message.file)) {
+						message = message.error || message.file;
+					} else if (typeof message !== "string") {
+					// Custom error sent by api
+						var finalMessage: string[] = [];
 
-                  // CUSTOM : Iterate on each message
-                  if (message.message.length > 0) {
-                    message.message.forEach((currentMessage: any) => {
-                      var errorList: string[] = [];
-                      // Iterate on each errors
-                      currentMessage.errors.forEach((errorMessage: string) => {
-                        errorList.push((currentMessage.row ? "Ligne " + currentMessage.row + " : " : "") + errorMessage + " \n ");
-                      })
-                      // Return result message
-                      finalMessage = finalMessage.concat(errorList);
-                    });
+						// CUSTOM : Iterate on each message
+						if (message.message.length > 0) {
+							message.message.forEach((currentMessage: any) => {
+								var errorList: string[] = [];
+								// Iterate on each errors
+								currentMessage.errors.forEach((errorMessage: string) => {
+									errorList.push((currentMessage.row ? "Ligne " + currentMessage.row + " : " : "") + errorMessage + " \n ");
+								})
+								// Return result message
+								finalMessage = finalMessage.concat(errorList);
+							});
 
-                    message = "<p>" + finalMessage.sort().join("</p><p>") + "</p>";
+							message = "<p>" + finalMessage.sort().join("</p><p>") + "</p>";
 
-                  } else {
-                    message = "Une erreur est surevenue, veuillez vérifier le fichier importé.";
-                  }
+						} else {
+							message = "Une erreur est surevenue, veuillez vérifier le fichier importé.";
+						}
 
-                }
+					}
 
-                // CUSTOM : add span data-dz-errormessage-index
-                const divFileError = $("<div id='dropzone-info' class='alert alert-danger' role='alert' data-dz-errormessage-" + file.upload.uuid + ">" + message + "</div>");
-                $('#dropzone-error-list').append($(divFileError));
-              }
-            }
+					// CUSTOM : add span data-dz-errormessage-index
+					const divFileError = $("<div id='dropzone-info' class='alert alert-danger' role='alert' data-dz-errormessage-" + file.upload.uuid + ">" + message + "</div>");
+					$('#dropzone-error-list').append($(divFileError));
+				}
+
+			  	setIsInfoHidden(true);
+		  		setIsHidden(true);
+            },
+			success: (file: any) => {
+				// Hide custom progress bar when processing
+				$(file.previewElement).find(".custom-dz-upload").hide();
+				// Override css success mark
+				$(file.previewElement).find(".dz-success-mark").css("animation", "slide-in 3s cubic-bezier(0.77, 0, 0.175, 1)");
+				$(file.previewElement).find(".dz-success-mark").css("opacity", "1");
+		
+				if (!autoProcessQueue) {
+				  // ManualProcess context : Is there file left to process ?
+				  if (dropZone!.getQueuedFiles().length > 0) {
+					dropZone!.processQueue();
+				  } else {
+					setIsInfoHidden(false);
+				  }
+				} else {
+				  // AutoProcess context : Refresh button available
+				  setIsHidden(false);
+				  setIsInfoHidden(true);
+				}
+		
+				addCallBack && addCallBack();
+			},
+			processing: (file: any) => {
+				setIsHidden(true);
+				setIsInfoHidden(true);
+				// Hide default progress bar when processing
+				$(file.previewElement).find(".dz-upload").hide();
+				// Show custom progress bar when processing
+				$(file.previewElement).find(".custom-dz-upload").show();
+				$(file.previewElement).find(".dz-progress").css("height", "inherit");
+			},
+			addedfiles: () => {
+				if (!autoProcessQueue) {
+					// Hide custom progress bar when add files
+					$("#upload_form-" + dropzoneIndex + " .custom-dz-upload").hide();
+					setIsHidden(false);
+					setIsInfoHidden(true);
+				}
+			},
+			sending: (file: any, _xhr, formData) => {
+				if (dropZoneParameter.chunking) {
+					formData.append('dzuuid', file.upload?.uuid);
+					formData.append('dztotalchunkcount', file.upload?.totalChunkCount);
+		
+					let index = 1;
+					// @ts-nocheck
+					if(file.upload?.chunks)
+					{
+					  index = file.upload.chunks[file.upload.chunks.length - 1].index
+					}
+					
+					formData.append('dzchunkindex', index.toString());
+					formData.append('dztotalfilesize', file.size );
+					formData.append('dzchunksize', dropZoneParameter.chunkSize.toString());
+				}
+			}
+
           }
         ));
       }
