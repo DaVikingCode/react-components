7959ec77284764f7f919551fd545315395ca4365
Camille Meunier <camille.meunier@davikingcode.com>
2024-04-12 14:10:23 +0200

    fix: lineloader -> set show methods as nullable

--- src/components/LineLoader.tsx
+++ src/components/LineLoader.tsx
@@ -2,42 +2,51 @@ import { FC, useState } from "react";
 import { LinearProgress, createStyles, withStyles } from "@material-ui/core";
 
 interface LoaderProps {
-  show: (value: boolean) => void;
+	show?: (value: boolean) => void;
 }
 
 const StyledLinear = withStyles(() =>
-  createStyles({
-    root: {
-      marginTop: "-1px",
-      position: "absolute",
-      width: "100%",
-      zIndex: 1000,
-    },
-    colorPrimary: {
-      backgroundColor: "var(--clr-primary)",
-    },
-    bar: {
-      backgroundColor: "var(--clr-secondary)",
-    },
-  })
+	createStyles({
+		root: {
+			marginTop: "-1px",
+			position: "absolute",
+			width: "100%",
+			zIndex: 1000,
+		},
+		colorPrimary: {
+			backgroundColor: "var(--clr-primary)",
+		},
+		bar: {
+			backgroundColor: "var(--clr-secondary)",
+		},
+	})
 )(LinearProgress);
 
 export const LineLoader: FC<LoaderProps> = ({ show }) => {
-  const [colorLineProgress, setColorLineProgress] = useState<"primary" | "secondary" | undefined>("primary" );
-  const [variant, setVariant] = useState<"determinate" | "indeterminate" | "buffer" | "query" | undefined>("determinate" )
+	const [colorLineProgress, setColorLineProgress] = useState<
+		"primary" | "secondary" | undefined
+	>("primary");
+	const [variant, setVariant] = useState<
+		"determinate" | "indeterminate" | "buffer" | "query" | undefined
+	>("determinate");
 
-  window.addEventListener("openLoader", () => {
-    show(true);
-    setColorLineProgress("primary");
-    setVariant("indeterminate");
-    
-  });
-  window.addEventListener("closeLoader", () => {
-    show(false);
-    setColorLineProgress("secondary");
-    setVariant("determinate");
-  });
-  return <StyledLinear color={colorLineProgress} variant={variant} value={100} />;
+	window.addEventListener("openLoader", () => {
+		if (show != undefined) {
+			show(true);
+		}
+		setColorLineProgress("primary");
+		setVariant("indeterminate");
+	});
+	window.addEventListener("closeLoader", () => {
+		if (show != undefined) {
+			show(false);
+		}
+		setColorLineProgress("secondary");
+		setVariant("determinate");
+	});
+	return (
+		<StyledLinear color={colorLineProgress} variant={variant} value={100} />
+	);
 };
 
 export default LineLoader;
