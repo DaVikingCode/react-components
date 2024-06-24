df73a13e91207ab36210b3e26423c93a68d6edd4
Camille Meunier <camille.meunier@davikingcode.com>
2024-06-24 11:33:34 +0200

    fix: edit min height for sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,73 +4,73 @@ import styled from "styled-components";
 import { AppIcon } from "./AppIcon";
 
 const Header = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 16px;
-  min-height: 48px;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding: 16px;
+	min-height: 62px;
 `;
 
-const SidebarWrapper = styled.aside<{ closed: boolean, isPhone: boolean}>`
-  display: flex;
-  flex-direction: column;
-  z-index: 2;
-  background: white;
-  height: 100%;
-  overflow-x: hidden;
-  transition: width 125ms ease-in;
-  ${(p) => { 
-    if (!p.closed) {
-      return p.isPhone ? `width: 100vw;` : `width: 340px;`
-    } else {
-      return `width: 0px;`
-    }
-  }}
-  box-shadow: ${(p) => (p.closed ? "" : "1px 0px 4px 1px #bbb")};
+const SidebarWrapper = styled.aside<{ closed: boolean; isPhone: boolean }>`
+	display: flex;
+	flex-direction: column;
+	z-index: 2;
+	background: white;
+	height: 100%;
+	overflow-x: hidden;
+	transition: width 125ms ease-in;
+	${(p) => {
+		if (!p.closed) {
+			return p.isPhone ? `width: 100vw;` : `width: 340px;`;
+		} else {
+			return `width: 0px;`;
+		}
+	}}
+	box-shadow: ${(p) => (p.closed ? "" : "1px 0px 4px 1px #bbb")};
 `;
 
 export interface SidebarProps {
-  open: boolean;
-  title: string;
-  searchBar?: ReactNode;
-  isPhone: boolean;
-  onClose: () => void;
+	open: boolean;
+	title: string;
+	searchBar?: ReactNode;
+	isPhone: boolean;
+	onClose: () => void;
 }
 
 export const Sidebar: FC<SidebarProps> = ({
-  children,
-  title,
-  searchBar,
-  onClose,
-  isPhone,
-  open = false,
-  ...props
+	children,
+	title,
+	searchBar,
+	onClose,
+	isPhone,
+	open = false,
+	...props
 }) => {
-  return (
-    <SidebarWrapper closed={!open} isPhone={isPhone} {...props}>
-      <Header>
-        {searchBar}
-        {title !== "" && (
-          <>
-            <Divider id="divideSearchBar" />
-            <Typography variant="overline">{title}</Typography>
-          </>
-        )}
-        {isPhone && open && (
-          <IconButton
-            style={{ width: "40px", height: "40px" }}
-            size="small"
-            onClick={onClose}
-          >
-            <AppIcon name="chevron-left" />
-          </IconButton>
-        )}
-      </Header>
+	return (
+		<SidebarWrapper closed={!open} isPhone={isPhone} {...props}>
+			<Header>
+				{searchBar}
+				{title !== "" && (
+					<>
+						<Divider id="divideSearchBar" />
+						<Typography variant="overline">{title}</Typography>
+					</>
+				)}
+				{isPhone && open && (
+					<IconButton
+						style={{ width: "40px", height: "40px" }}
+						size="small"
+						onClick={onClose}
+					>
+						<AppIcon name="chevron-left" />
+					</IconButton>
+				)}
+			</Header>
 
-      <Divider />
-      {children}
-    </SidebarWrapper>
-  );
+			<Divider />
+			{children}
+		</SidebarWrapper>
+	);
 };
 
 export default Sidebar;
